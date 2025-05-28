7c00a4d0eb57efe188f0cc62b19c7c2d7b01c818
Eric DUQUENOY <eric.duquenoy.pro@gmail.com>
2025-05-28 10:38:20 +0200

    Update nikplugin.py
    MacOs don't wait program terminaison, need to force them

--- nikplugin.py
+++ nikplugin.py
@@ -380,7 +380,7 @@ def run_nik(prog_idx: int, images: List[Gimp.Image]) -> Optional[str]:
 
         # Run the external program
         if sys.platform == "darwin":
-            prog_caller = ["open", "-a"]
+            prog_caller = ["open", "-W", "-a"] # MacOs don't wait program terminaison, need to force them
         elif sys.platform == "linux":
             prog_caller = ["wine"]
         else:  # windows
